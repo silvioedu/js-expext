f44feeacf3dc2454ebf6650212780dfaac3377a6
Silvio Silva <silvioedu@gmail.com>
2023-01-25 16:38:24 -0300

    aula03

---
+++ aula03-spies/src/fibonacci.test.js
@@ -0,0 +1,40 @@
+const Fibonacci = require('./fibonacci')
+const sinon = require('sinon')
+const assert = require('assert')
+
+;
+(async() => {
+  {
+    const fibonacci = new Fibonacci()
+    const spy = sinon.spy(fibonacci, fibonacci.execute.name)
+    
+    const expectedCallCount = 4
+    for (const i of fibonacci.execute(3)) {}
+
+    assert.deepStrictEqual(spy.callCount, expectedCallCount)
+  }
+  {
+    const fibonacci = new Fibonacci()
+    const spy = sinon.spy(fibonacci, fibonacci.execute.name)
+    
+    const [...results] = fibonacci.execute(5)
+    
+    // [0] input = 5, current = 0, next = 1
+    // [1] input = 4, current = 1, next = 1
+    // [2] input = 3, current = 1, next = 2
+    // [3] input = 2, current = 2, next = 3
+    // [4] input = 1, current = 3, next = 5
+    // [5] input = 0 -> PARA
+    const { args } = spy.getCall(2)
+    const expectedParams = Object.values({
+      input: 3,
+      current: 1,
+      next: 2
+    })
+    const expectedResult = [0, 1, 1, 2, 3]
+
+    assert.deepStrictEqual(args, expectedParams)
+    assert.deepStrictEqual(results, expectedResult)
+
+  }
+})()
